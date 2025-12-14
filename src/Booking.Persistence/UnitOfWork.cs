b1df67dd059cf4f043553033c15c112e1a23a91d
tuanln <lenguyentuan98@gmail.com>
2025-12-14 23:57:50 +0700

    implement unit of work

---
+++ src/Booking.Persistence/UnitOfWork.cs
@@ -0,0 +1,18 @@
+using Booking.Application.Common;
+
+namespace Booking.Persistence;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public UnitOfWork(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        return await _dbContext.SaveChangesAsync(cancellationToken);
+    }
+}
